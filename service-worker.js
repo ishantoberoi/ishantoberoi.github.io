645a0d331a3976fb0413199d97260bc784f04fc7
ishantoberoi <ishant.oberoi@snapdeal.com>
2016-07-26 12:41:47 +0530

    event spell

--- service-worker.js
+++ service-worker.js
@@ -19,7 +19,7 @@ var filesToCache = [
   '/images/wind.png'  
 ];
 
-self.addEvenListener('install', function (e) {
+self.addEventListener('install', function (e) {
 	console.log('[Service Worker] Install');
 	e.waitUntil(
 		caches.open(cacheName).then(function(cache) {
@@ -29,7 +29,7 @@ self.addEvenListener('install', function (e) {
 	);
 });
 
-self.addEvenListener('activate', function (e) {
+self.addEventListener('activate', function (e) {
 	console.log('[ServiceWorker] Activate');
 	e.waitUntil(
 			caches.keys().then(function (keyList) {
@@ -42,7 +42,7 @@ self.addEvenListener('activate', function (e) {
 		)
 });
 
-self.addEvenListener('fetch', function (e) {
+self.addEventListener('fetch', function (e) {
 	console.log('[ServiceWorker] fetch', e.request.url);
 	e.respondWith(
 			caches.match(e.request).then(function (response) {
