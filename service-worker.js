6d6a06598621c77d52e6d0fdacf77794d9da4d0a
ishantoberoi <ishant.oberoi@snapdeal.com>
2016-07-26 14:19:46 +0530

    Offline support

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var cacheName = 'weatherPWA-step-5-3';
+var cacheName = 'weatherPWA-step-5-4';
 var filesToCache = [  
   '/',  
   '/index.html',  
@@ -18,6 +18,8 @@ var filesToCache = [
   '/images/thunderstorm.png',  
   '/images/wind.png'  
 ];
+
+var dataCacheName = 'weatherData-v1';
 
 self.addEventListener('install', function(e) {  
   console.log('[ServiceWorker] Install');  
@@ -44,10 +46,23 @@ self.addEventListener('activate', function(e) {
 });
 
 self.addEventListener('fetch', function(e) {  
-  console.log('[ServiceWorker] Fetch', e.request.url);  
-  e.respondWith(  
-    caches.match(e.request).then(function(response) {  
-      return response || fetch(e.request);  
-    })  
-  );  
+  console.log('[ServiceWorker] Fetch', e.request.url); 
+  var dataUrl = 'https://publicdata-weather.firebaseio.com/';
+  if(e.request.url.indexOf(dataUrl) === 0) {
+  	e.respondWith(
+  			fetch(e.request).then(function (response) {
+  				return caches.open(dataCacheName).then(function (cache) {
+  					cache.put(e.request.url, response.clone());
+  					console.log('[ServiceWorker] Fetched&Cached Data');
+  					return response;
+  				})
+  			})
+  		)
+  } else {
+	  	e.respondWith(  
+	    caches.match(e.request).then(function(response) {  
+	      return response || fetch(e.request);  
+	    })  
+	  ); 	
+  } 
 });
