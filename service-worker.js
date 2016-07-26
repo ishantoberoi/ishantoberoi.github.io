6e71ce85557b32222ba621034d9668fa68dcebce
ishantoberoi <ishant.oberoi@snapdeal.com>
2016-07-26 12:43:50 +0530

    events in worker

--- service-worker.js
+++ service-worker.js
@@ -19,34 +19,35 @@ var filesToCache = [
   '/images/wind.png'  
 ];
 
-self.addEventListener('install', function (e) {
-	console.log('[Service Worker] Install');
-	e.waitUntil(
-		caches.open(cacheName).then(function(cache) {
-			console.log('[ServiceWorker] Caching App Shell');
-			return cache.addAll(filesToCache);
-		});
-	);
+self.addEventListener('install', function(e) {  
+  console.log('[ServiceWorker] Install');  
+  e.waitUntil(  
+    caches.open(cacheName).then(function(cache) {  
+      console.log('[ServiceWorker] Caching app shell');  
+      return cache.addAll(filesToCache);  
+    })  
+  );  
 });
 
-self.addEventListener('activate', function (e) {
-	console.log('[ServiceWorker] Activate');
-	e.waitUntil(
-			caches.keys().then(function (keyList) {
-				return Promise.all(keyList.map(function (key) {
-					if(key !== cacheName) {
-						return caches.delete(key);
-					}
-				}))
-			})
-		)
+self.addEventListener('activate', function(e) {  
+  console.log('[ServiceWorker] Activate');  
+  e.waitUntil(  
+    caches.keys().then(function(keyList) {  
+      return Promise.all(keyList.map(function(key) {  
+        console.log('[ServiceWorker] Removing old cache', key);  
+        if (key !== cacheName) {  
+          return caches.delete(key);  
+        }  
+      }));  
+    })  
+  );  
 });
 
-self.addEventListener('fetch', function (e) {
-	console.log('[ServiceWorker] fetch', e.request.url);
-	e.respondWith(
-			caches.match(e.request).then(function (response) {
-				return response || fetch(e.request);
-			});
-		);
+self.addEventListener('fetch', function(e) {  
+  console.log('[ServiceWorker] Fetch', e.request.url);  
+  e.respondWith(  
+    caches.match(e.request).then(function(response) {  
+      return response || fetch(e.request);  
+    })  
+  );  
 });
